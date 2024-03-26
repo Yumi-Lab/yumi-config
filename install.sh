7bf40ab173984685e0662a4b89f86750328d958b
Yumi-Lab <nicolas@yumi-lab.com>
2024-03-26 14:43:36 +0800

    update

--- install.sh
+++ install.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-# Vérifiez si le script est exécuté avec sudo et déterminez l'utilisateur réel
-if [ ! -z "$SUDO_USER" ]; then
-    REAL_USER="$SUDO_USER"
-else
-    REAL_USER="$(whoami)"
-fi
-echo "Utilisateur réel: $REAL_USER"
+# Récupérer l'utilisateur qui exécute le script
+REAL_USER="$USER"
 
-# Utilisez getent pour obtenir le chemin du répertoire personnel de l'utilisateur réel
-USER_HOME=$(getent passwd "$REAL_USER" | cut -d: -f6)
-echo "Répertoire personnel de l'utilisateur: $USER_HOME"
+# Initialisation de la variable OWNER
+OWNER=""
+
+# Récupérer le répertoire de l'utilisateur
+if [ -n "$SUDO_USER" ]; then
+    echo "shell script execute by with sudo :  user is $SUDO_USER"
+    if [ "$SUDO_USER" = "runner" ]; then
+        # Définir USER_HOME spécifiquement pour 'runner' et définir OWNER à 'pi'
+        USER_HOME="/home/pi"
+        OWNER="pi"
+    else
+        USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+        OWNER="$SUDO_USER"
+    fi
+else
+    USER_HOME=$(getent passwd "$USER" | cut -d: -f6)
+    OWNER="$USER"
+    echo "shell script execute without sudo : user is $USER"
+fi
+
+echo "Real user: $REAL_USER"
+echo "User's home directory: $USER_HOME"
+echo "Owner for chown: $OWNER"
+
+# Define the Klipper directory using USER_HOME instead of HOME
 KLIPPER_DIR="$USER_HOME/klipper"
-echo "Répertoire Klipper: $KLIPPER_DIR"
+echo "Klipper directory: $KLIPPER_DIR"
+
+# Define the project directory
 PROJECT_DIR="$PWD"
-echo "Répertoire du projet: $PROJECT_DIR"
+echo "Project directory: $PROJECT_DIR"
+
 KLIPPER_CONFIG_DIR="$KLIPPER_DIR/printer_data/config"
 echo "Répertoire du config: $KLIPPER_CONFIG_DIR"
 
@@ -25,8 +45,11 @@ function install {
   cp "$PROJECT_DIR/smartpad-generic/smartpad-cpu-temp.cfg" "$KLIPPER_CONFIG_DIR" && echo "smartpad-cpu-temp.cfg copié avec succès." || echo "Erreur lors de la copie de smartpad-cpu-temp.cfg."
   rm -f "$KLIPPER_CONFIG_DIR/smartpad-adxl345.cfg" && echo "smartpad-adxl345.cfg supprimé avec succès." || echo "Erreur lors de la suppression de smartpad-adxl345.cfg."
   cp "$PROJECT_DIR/smartpad-generic/smartpad-adxl345.cfg" "$KLIPPER_CONFIG_DIR" && echo "smartpad-adxl345.cfg copié avec succès." || echo "Erreur lors de la copie de smartpad-adxl345.cfg."
+ 
+  if [ "$1" == "smartpad-generic" ]; then
   rm -f "$KLIPPER_CONFIG_DIR/printer.cfg" && echo "printer.cfg supprimé avec succès." || echo "Erreur lors de la suppression de printer.cfg."
   cp "$PROJECT_DIR/$1/printer.cfg" "$KLIPPER_CONFIG_DIR" && echo "printer.cfg copié avec succès." || echo "Erreur lors de la copie de printer.cfg."
+  fi
 
   # Modifier les permissions pour que l'utilisateur "pi" conserve les droits sur les fichiers créés ou modifiés
   chown -R pi:pi "$KLIPPER_CONFIG_DIR"
