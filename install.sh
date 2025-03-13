49bebe8f0f1b6adc37b93a4b54c343e0b148310d
Maxime3d77 <114477279+Maxime3d77@users.noreply.github.com>
2025-03-13 11:04:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -163,8 +163,11 @@ echo "Nouveau lien symbolique créé : $SYMLINK_TARGET → $PANEL_SCRIPT"
 if [[ -f "$FILE" ]]; then
     echo "Modification du fichier : $FILE"
 
+    # Remplacement lien doc
+    sed -i "s|self.update_qr_code('https://obico.io/docs/user-guides/klipper-setup/')|self.update_qr_code('https://wiki.yumi-lab.com/KlipperSmartPad/SmartPad_Yumi_App/')|g" "$FILE"
+
     # Remplacement de box_size=4 par box_size=12
-    sed -i 's/box_size=4/box_size=12/g' "$FILE"
+    sed -i 's/box_size=4/box_size=6/g' "$FILE"
 
     # Remplacement de back_color="white" par back_color="grey"
     sed -i 's/img = qr.make_image(fill_color="black", back_color="white")/img = qr.make_image(fill_color="grey", back_color="black")/g' "$FILE"
